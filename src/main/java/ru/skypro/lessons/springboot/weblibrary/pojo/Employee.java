3f21a2a47263071be157b06e9b5f66dcd733d613
klozz <ya.slava2323yaru@gmail.com>
2023-05-03 22:44:40 +0300

    test

---
+++ src/main/java/ru/skypro/lessons/springboot/weblibrary/pojo/Employee.java
@@ -0,0 +1,19 @@
+package ru.skypro.lessons.springboot.weblibrary.pojo;
+
+public class Employee {
+    private String name;
+    private int salary;
+
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+}
